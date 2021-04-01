b34dfb5031b9c25277df7781a66d072a179734f1
Nadezhda <sakaln88@gmail.com>
2021-04-01 23:01:15 +0300

    remove comments

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -3,16 +3,6 @@
 public class Palindrom {
     public static void main(String[] args) {
 
-
-//        int a = 0;
-//        PalindromeLib pLib = new PalindromeLib();
-//        while (a < 1000) {
-//            if (pLib.isPalindrome(String.valueOf(a))) {
-//                System.out.println(a);
-//            }
-//            a++;
-//
-//        }
         int a = 0;
         palindromeStorage storage = new palindromeStorage();
         while (a < 1000) {
